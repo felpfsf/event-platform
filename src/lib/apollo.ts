6cdc11cf5b2501293a201d1861afd00f9cad4463
felpfsf <feferreira.sf@gmail.com>
2022-06-23 19:38:30 -0300

    correcting bug with video cache in apolloClient

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,9 +1,21 @@
 // npm i @apollo/client graphql
 
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, DefaultOptions, InMemoryCache } from '@apollo/client'
+
+const defaultOptions: DefaultOptions = {
+  watchQuery: {
+    fetchPolicy: 'no-cache',
+    errorPolicy: 'ignore'
+  },
+  query: {
+    fetchPolicy: 'no-cache',
+    errorPolicy: 'all'
+  }
+}
 
 export const client = new ApolloClient({
   uri: 'https://api-sa-east-1.graphcms.com/v2/cl4o8rvvm16l201z7e5l20kbe/master',
   //  Por padrão o Apollo armazena o cache dentro da aplicação
-  cache: new InMemoryCache()
+  cache: new InMemoryCache(),
+  defaultOptions: defaultOptions
 })
