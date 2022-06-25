4f0ca1a57906656d8fa73974cd72dae655ee69c9
felpfsf <feferreira.sf@gmail.com>
2022-06-25 18:21:32 -0300

    loading page stat

--- src/pages/Loading.tsx
+++ src/pages/Loading.tsx
@@ -1,17 +1,26 @@
-import { ReactIcon } from '../components/ReactIcon'
+import reactjsIcon from '../assets/reactjs-icon.svg'
+import reactLogo from '../assets/react-logo.svg'
+import { Footer } from '../components/Footer'
 
 export function Loading() {
   return (
     <div className='flex-1'>
-      <div className='md:bg-bgBlur sm:bg-bgBlurMob sm:wbg-cover bg-no-repeat h-1/2 mx-8 my-8 py-16'>
-        <span className='absolute left-10 top-[120px] overflow-clip'>
-          <ReactIcon />
-        </span>
-        <div className='flex flex-col items-center justify-center'>
-          <h1>Welcome to Ignit'n Minds</h1>
-          <p>We're going to develop a full application on React JS</p>
+      <div className='md:bg-bgBlur sm:bg-bgBlurMob sm:wbg-cover bg-no-repeat h-1/2 mx-8 my-8 py-16 flex flex-col items-center gap-6'>
+        <img src={reactLogo} className='w-40 animate-spin-slow'></img>
+        <div className='p-4 flex flex-col items-center justify-center gap-4'>
+          <h1 className='sm:text-2xl md:text-[40px] leading-tight sm:text-center md:text-left text-orange-500'>
+            Welcome to Ignit'n Minds
+          </h1>
+          <p className='sm:text-base md:text-base leading-relaxed text-gray-100 sm:text-center md:text-left'>
+            We're going to develop a full application on React JS
+          </p>
+          <p className='sm:text-base md:text-base leading-relaxed text-blue-500 sm:text-center md:text-left animate-pulse'>
+            {' '}
+            Choose a lesson to start{' '}
+          </p>
         </div>
       </div>
+      <Footer />
     </div>
   )
 }
